aabe4bc34f21512c4546ce800fde838e909e6290
Daniel Dudin <daniel@forkpoint.com>
2020-04-19 20:35:03 +0300

    v0.3 - Having an actual test level environment, folders structure re-organise

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    UnityEngine.AI.NavMeshAgent _navMeshAgent;
-
-    public GameObject placedTarget;
+    public NavMeshAgent playerAgent;
     private PhotonView PV;
 
     public Animator playerAnimator;
@@ -21,11 +20,9 @@ public class PlayerMovement : MonoBehaviour
         playerAnimator = GetComponent<Animator>();
 
         if (PV.IsMine) {
-            placedTarget = GameObject.Find("TargetPointer").GetComponent<PlaceTarget>().gameObject;
+            playerAgent = this.GetComponent<NavMeshAgent>();
 
-            _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
-
-            if (_navMeshAgent == null)
+            if (playerAgent == null)
             {
                 Debug.Log("The nav mesh agent component is not attached to the Character");
             }
@@ -34,26 +31,68 @@ public class PlayerMovement : MonoBehaviour
 
     void Update() {
         if (PV.IsMine) {
-            if (Vector3.Distance(_navMeshAgent.destination, placedTarget.transform.position) > 0.5f)
-            {
-                playerAnimator.SetInteger("condition", 1);
-                playerAnimator.SetBool("running", true);
 
-                _navMeshAgent.SetDestination(placedTarget.transform.position);
-            } 
-            else if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            if (!playerAgent.pathPending)
             {
-                if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f)
+                if (playerAgent.remainingDistance <= playerAgent.stoppingDistance)
                 {
-                    playerAnimator.SetInteger("condition", 0);
-                    playerAnimator.SetBool("running", false);
+                    if (!playerAgent.hasPath || playerAgent.velocity.sqrMagnitude == 0f)
+                    {
+                        playerAnimator.SetBool("running", false);
+                        playerAnimator.SetInteger("condition", 0);
+                    }
                 }
             }
 
-            if (Input.GetKey(KeyCode.Alpha1)) {
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
                 Attack();
             }
+
+            if (!Input.GetMouseButton(0) || PointerOverUI())
+            {
+                return;
+            }
+
+            GetInteraction();
         }
+    }
+
+    void GetInteraction() {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity)) 
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+
+            if (interactedObject.tag == "InteractableObject") 
+            {
+
+            }
+            else 
+            {
+                playerAgent.destination = interactionInfo.point;
+
+                playerAnimator.SetInteger("condition", 1);
+                playerAnimator.SetBool("running", true);
+            }
+        }
+    }
+
+    public bool PointerOverUI()
+    {
+        return UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
+
     }
 
     void Attack() {
---
+++ Assets/Scripts/World/Interactable.cs
@@ -0,0 +1,15 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour
+{
+    public NavMeshAgent playerAgent;
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent) {
+        this.playerAgent = playerAgent;
+    }
+}
